ccf3835e3e91f8c751df3b27e623b3a74a3f7718
kdaespir <84219248+kdaespir@users.noreply.github.com>
2023-02-06 20:10:04 -0500

    Update COPD_pred.py

--- COPD_pred.py
+++ COPD_pred.py
@@ -6,4 +6,16 @@ from sklearn.linear_model import LogisticRegression
 
 # Loads the dataset
 data = pd.read_csv("dataset.csv")
+
+# drops the irrelevant features
+data = data.drop(['Unnamed: 0', 'ID'], axis=1)
+
+# Replaces the text classifications for COPD severity with numerical ones
+severity = data["COPDSEVERITY"]
+severity = severity.replace({"MILD": 0, "MODERATE" : 1, "SEVERE" : 2, "VERY SEVERE" : 3})
+data["COPDSEVERITY"] = severity
+
+data_x = data.drop(["COPDSEVERITY"], axis=1)
+data_y = data["COPDSEVERITY"]
+
 print(data.head())
